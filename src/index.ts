c8f81a4f700c25fa5ba92a8ad8f3fc051d6dcfd7
h7ml <h7ml@qq.com>
2024-05-01 19:11:24 +0800

    ✨ feat: Use tsc

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
-import { getInput, setFailed, setOutput } from '@actions/core';
-import { Octokit } from '@octokit/rest';
-import { context } from '@actions/github';
+import { getInput, setFailed, setOutput } from '@actions/core'
+import { Octokit } from '@octokit/rest'
+import { context } from '@actions/github'
 
 async function run() {
   try {
-    const token = getInput('token');
-    const octokit = new Octokit({ auth: `token ${token}` });
-    const { owner, repo } = context.repo;
-    const body = getInput('body');
+    const token = getInput('token')
+    const octokit = new Octokit({ auth: `token ${token}` })
+    const { owner, repo } = context.repo
+    const body = getInput('body')
 
     const response = await octokit.repos.createCommitComment({
       owner,
       repo,
       commit_sha: context.sha,
       body,
-    });
-    const comment_id = response.data.id;
+    })
+    const comment_id = response.data.id
 
-    setOutput('comment_id', comment_id);
+    setOutput('comment_id', comment_id)
   } catch (error: any) {
-    setFailed(error.message);
+    setFailed(error.message)
   }
 }
 
-run().catch(error => {
-  setFailed(error.message);
-});
+run()
