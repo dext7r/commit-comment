97c6aa85a2b101228c6cebf1302673f855e96a32
h7ml <h7ml@qq.com>
2024-05-01 16:28:50 +0800

    👷 ci(action): Done

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export default function () {
-  return 'hello world';
+import { getInput, setFailed, setOutput } from '@actions/core';
+import { Octokit } from '@octokit/rest';
+import { context } from '@actions/github';
+
+async function run() {
+  try {
+    const token = getInput('token');
+    const octokit = new Octokit({ auth: `token ${token}` });
+    const { owner, repo } = context.repo;
+    const body = getInput('body');
+
+    const response = await octokit.repos.createCommitComment({
+      owner,
+      repo,
+      commit_sha: context.sha,
+      body,
+    });
+    const comment_id = response.data.id;
+
+    setOutput('comment_id', comment_id);
+  } catch (error: any) {
+    setFailed(error.message);
+  }
 }
+
+run().catch(error => {
+  setFailed(error.message);
+});
